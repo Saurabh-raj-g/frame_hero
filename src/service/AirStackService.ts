e5a3739f7088e3c68254f38fbfdd23003ee8c6af
Hridayesh <43112749+Hridayesh13@users.noreply.github.com>
2024-03-25 02:16:13 +0900

    minor fix

--- src/service/AirStackService.ts
+++ src/service/AirStackService.ts
@@ -4,31 +4,15 @@ import axios from 'axios';
 export default class AirStackService {
   
 
-    // public static async userByFid(fid:number) {
-    //     const input: FarcasterUserDetailsInput = {
-    //         fid: 602,
-    //     };
-    //     const { data, error }: FarcasterUserDetailsOutput =
-    //     await getFarcasterUserDetails(input);
-        
-    //     if (error) throw new Error(error);
-    //     return data;
-    // }
-
-
     public static async userByFid(fid:number) {
-
-        const options = {
-            method: 'GET',
-            url: `https://api.neynar.com/v2/farcaster/user/bulk?fids=${fid}`,
-            headers: {accept: 'application/json', api_key: process.env.NYNAR_API_KEY}
-          };
-          
-          const response = await axios
-            .request(options);
-
-            return response.data;
-           
+        const input: FarcasterUserDetailsInput = {
+            fid: 602,
+        };
+        const { data, error }: FarcasterUserDetailsOutput =
+        await getFarcasterUserDetails(input);
+        
+        if (error) throw new Error(error);
+        return data;
     }
     }
    
