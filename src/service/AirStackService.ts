5b523580556ad081ecc634b1a5b725e9363ab212
Saurabh-raj-g <saurabhraj14499@gmail.com>
2024-03-28 16:02:25 +0900

    minor change

--- src/service/AirStackService.ts
+++ src/service/AirStackService.ts
@@ -4,15 +4,15 @@ import axios from 'axios';
 export default class AirStackService {
   
 
-    public static async userByFid(fid:number) {
-        const input: FarcasterUserDetailsInput = {
-            fid: 602,
-        };
-        const { data, error }: FarcasterUserDetailsOutput =
-        await getFarcasterUserDetails(input);
+    // public static async userByFid(fid:number) {
+    //     const input: FarcasterUserDetailsInput = {
+    //         fid: 602,
+    //     };
+    //     const { data, error }: FarcasterUserDetailsOutput =
+    //     await getFarcasterUserDetails(input);
         
-        if (error) throw new Error(error);
-        return data;
-    }
+    //     if (error) throw new Error(error);
+    //     return data;
+    // }
     }
    
